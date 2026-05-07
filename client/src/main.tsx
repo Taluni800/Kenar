a628faffa020cf8913eb3a1a68931b0a9d848a14
taluni800 <mykytaro@proton.me>
2026-05-07 08:17:00 +0300

    creat a router in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/sidebar", element: <Sidebar /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);

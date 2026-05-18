04a7eabfad0f3098e49ca3f6eccae97522fe0e03
taluni800 <mykytaro@proton.me>
2026-05-18 16:59:44 +0300

    put dashboard link outside nav tag

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -4,12 +4,11 @@ import { Link } from "react-router-dom";
 function Sidebar() {
   return (
     <aside className="sidebar">
+      <Link to="/dashboard">
+        <li className="sidebar-item active">Dashboard</li>
+      </Link>
       <nav className="sidebar-nav">
-        <ul>
-          <Link to="/dashboard">
-            <li className="sidebar-item active">Dashboard</li>
-          </Link>
-        </ul>
+        <ul></ul>
       </nav>
     </aside>
   );

af9bef7c9af1edbe7f78c555bae7034a01a6b2c4
taluni800 <mykytaro@proton.me>
2026-05-20 13:51:39 +0300

    add modifying content of the notes

--- client/src/components/NotePage/NotePage.tsx
+++ client/src/components/NotePage/NotePage.tsx
@@ -2,16 +2,47 @@ import { useParams } from "react-router-dom";
 import { db } from "../../db";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
 import { useLiveQuery } from "dexie-react-hooks";
+import "./NotePage.css";
+import { useEffect, useRef } from "react";
 
 function NotePage() {
   const params = useParams<{ noteID: string }>();
   const id: number = Number(params.noteID);
   const note = useLiveQuery(() => db.notes.get(id), [id]);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  async function saveNote() {
+    try {
+      if (inputRef.current) {
+        await db.notes.update(id, { content: inputRef.current.value });
+      }
+    } catch {
+      console.error("Problem with updating");
+    }
+  }
+
+  useEffect(() => {
+    if (inputRef.current && note) {
+      inputRef.current.value = note.content;
+    }
+  }, [note]);
+
   if (!note) return <NotFoundPage />;
 
   return (
     <div>
       <h1>{note.title}</h1>
+      <div className="inputs">
+        <textarea
+          name=""
+          id="textarea"
+          ref={inputRef}
+          placeholder="Loading..."
+        ></textarea>
+        <button onClick={saveNote} id="saveBtn">
+          Save
+        </button>
+      </div>
     </div>
   );
 }

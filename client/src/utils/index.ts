ee9b759ce796aa663c7abbe37facdcd5b7cdb683
taluni800 <mykytaro@proton.me>
2026-05-21 07:56:19 +0300

    exported memoization function

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as log } from "./logger";
+export { default as memoization } from "./memoization";
--- client/src/utils/memoization.ts
+++ client/src/utils/memoization.ts
@@ -74,3 +74,5 @@ function memoization(
     return result;
   };
 }
+
+export default memoization;
